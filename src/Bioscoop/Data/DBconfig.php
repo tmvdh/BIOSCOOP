dd88fb16fbaf6a9452c6976ef31ec27ff1ce8f28
tmvdh <TomV@nderheijden.be>
2015-04-01 10:26:50 +0200

    init

---
+++ src/Bioscoop/Data/DBconfig.php
@@ -0,0 +1,7 @@
+<?php
+namespace Wijnhandel\Data;
+class DBConfig {
+public static $DB_CONNSTRING = "mysql:host=localhost;dbname=bioscoop";
+public static $DB_USERNAME = "website";
+public static $DB_PASSWORD = "website";
+}
